794aa670bfd8f85c77b44d653684d448a78329fe
nqv1208 <qvinhkl10@gmail.com>
2026-03-13 20:48:01 +0700

    add program

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("------------------------------");
+        Console.WriteLine("Thông tin sinh viên:");
+        Console.WriteLine("Họ tên: Nguyen Quang Vinh");
+        Console.WriteLine("MSV: 23115053122346");
+        Console.WriteLine("Lớp: 23T3");
+        Console.WriteLine("Lớp học phần: Lập trình C#");
+        Console.WriteLine("------------------------------");
+
+    }
+}
